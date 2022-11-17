af7673de280c7ffcaf8cb2ecdd8b75e4e4126be4
Patrick Stöckle <patrick.stoeckle@posteo.de>
2022-11-17 11:36:20 +0100

    :construction_worker: Minify website

---
+++ build.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const htmlMinifier = require('html-minifier');
+const logError = (err2) => {
+    if (err2) {
+        console.error(err2);
+        return;
+    }
+}
+
+fs.readFile('_site/index.html', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const result = htmlMinifier.minify(data, {
+        minifyCSS: true,
+        collapseWhitespace: true,
+        removeComments: true
+    });
+    fs.writeFile('_site/index.html', result, {},logError )
+})
+
+fs.rm('_site/LICENSE', logError)
+fs.rm('_site/README.md', logError)
+fs.rm('_site/build.js', logError)
+fs.rm('_site/package.json', logError)
+fs.rm('_site/package-lock.json', logError)
